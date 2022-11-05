0561b19a73df9eefa51e79d1635feb07fa37fd4c
Edu <eduardo.sbassani@gmail.com>
2022-11-06 01:16:02 +0200

    Ron used my pc, idk what he did

--- src/main/java/com/hestia/app/project/ProjectController.java
+++ src/main/java/com/hestia/app/project/ProjectController.java
@@ -1,6 +1,7 @@
 package com.hestia.app.project;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,8 +22,11 @@ public class ProjectController {
     }
 
     @PostMapping("/create")
-    public void addProjects(@RequestBody Project project) {
-        projectService.addProject(project, 1L); // use Session ID
+    public void addProjects(@RequestBody MultiValueMap<String, String> request) {
+        Project project = new Project();
+        project.setName(request.getFirst("name"));
+        project.setDescription(request.getFirst("description"));
+        projectService.addProject(project, Long.getLong(request.getFirst("userId"))); // use Session ID
     }
 
     @DeleteMapping(path = "{projectId}")
